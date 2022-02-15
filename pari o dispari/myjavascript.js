e6266d00c309fbebf680e4b77ef2d4dfac53b7cf
edodima13 <edodima13@libero.it>
2022-02-15 18:44:54 +0100

    fine esercizio

--- pari o dispari/myjavascript.js
+++ pari o dispari/myjavascript.js
@@ -2,7 +2,7 @@
 const scelta = prompt("scegli pari o dispari");
 
 //chiedere all'utente di scegliere un numero da 1 a 5
-const userNumber = prompt("scegli un numero da 1 a 5");
+const userNumber = parseInt(prompt("scegli un numero da 1 a 5"));
 
 //generare un numero random che vada da 1 a 5 per il computer
 function getrandom(min,max) {
@@ -12,3 +12,35 @@ function getrandom(min,max) {
 }
 
 const pcNumero = getrandom(1,5)
+
+
+//fare la somma
+let somma = pcNumero + userNumber
+console.log(somma)
+
+// dire se ha scelto pari o dispari
+const sceltaPari = "pari"
+
+const sceltaDispari = "dispari"
+
+
+//decretare il vincitore
+if ( scelta === sceltaPari && somma % 2 === 0){
+    console.log( "hai vinto" )
+}  
+
+
+if ( scelta === sceltaDispari && somma % 3 === 0){
+    console.log( "hai vinto" )
+} else {
+    console.log( "ha vinto il computer")
+}
+
+
+
+
+/*
+else if ( scelta === sceltaDispari && somma % 3 === 0){
+    console.log( "hai vinto" )
+}
+*/
