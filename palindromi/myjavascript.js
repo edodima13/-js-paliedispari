130342b15c2ea5a991d164d69ecadac8d9aceede
edodima13 <edodima13@libero.it>
2022-02-15 15:13:34 +0100

    cambio approccio sanck-palindromo

--- palindromi/myjavascript.js
+++ palindromi/myjavascript.js
@@ -1,11 +1,31 @@
 //chiedere all'utente di inserire una parola
+const parola = prompt("inserisci una parola")
 
-let parola = prompt("inserisci una parola")
+
+//invertire parola
+function invertita(parola) {
+    return parola.split("").reverse().join("") 
+    
+}
+console.log(invertita(parola))
 
 //creare una funzione capace di capire se la parola inserita è palindroma
-if ( parola === parola.split("").reverse().join("") ){
-    console.log (" è palindroma")
-} else {
-    console.log(" non è palindroma")
+function  palindroma(parola) {
+    const reverse = invertita(parola)
+
+    if ( parola === reverse){
+        return true
+    } else {
+        return false
+    }
 }
+
+//stampare un messaggio di verifica per l'utente
+if ( palindroma(parola) ){
+    console.log("la parola inserita è palindroma")
+}   else {
+    console.log("la parola inserita non è palindroma")
+}
+
+
 
